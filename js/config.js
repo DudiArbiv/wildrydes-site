1fb0626426d6556660dfe85caa733965c4240b16
DudiArbiv <80258538+DudiArbiv@users.noreply.github.com>
2025-04-19 04:30:23 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_drSSwr1b3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3f9s438aic5o0oa5nuj5bq94om', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
